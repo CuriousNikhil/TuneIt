6405160085f5a75a1847bea25cf40b09975368e8
nikhya <nikhyl777@gmail.com>
2017-09-29 18:19:45 +0530

    Frequency class added

---
+++ tuneit/src/main/java/xyz/mystikolabs/tuneit/Frequency.java
@@ -0,0 +1,18 @@
+package xyz.mystikolabs.tuneit;
+
+/**
+ * Created by nikhya on 29/9/17.
+ *
+ * A constants frequency class as a utility for producing
+ * the octave frequency
+ */
+
+
+public class Frequency {
+
+
+
+
+
+
+}
